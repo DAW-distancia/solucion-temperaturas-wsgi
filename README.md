c9905208bde0f1f1cb67962a02a139a1344b708c
Luis Miguel <morillas@gmail.com>
2023-04-13 15:48:01 +0200

    mysql

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 Ejecutar:
 
-```bash
+```bash 
+$ vagrant plugin install vagrant-docker-compose
 $ vagrant up
 ```
+> La BBDD de SQLite estaba dando un error. En esta versión se usa MySQL en el programa Flask. Para ello se ha modificado el fichero `__init__.py` que tiene la cadena de conexión y se ha añadido un fichero `docker-compose.yml` para levantar el contenedor de MySQL.
 
 Navegar a `localhost:8080` o modificar en `Vagrantfile`
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,34 +2,45 @@
 
 # Instalación básica (+wsgi)
 apt-get update
-apt-get install -y apache2 python3-venv git libapache2-mod-wsgi-py3 
+apt-get install -y apache2 python3-venv git libapache2-mod-wsgi-py3
+# para mysqlclient
+apt-get install -y python3-dev default-libmysqlclient-dev build-essential
+
+#Inicio mysql
+
+cp /vagrant/docker-compose.yml /home/vagrant/
+cd /home/vagrant
+docker-compose  up -d
 
 # Preparación de la web
 cd /var/www
-git clone https://github.com/lmorillas/flask_temperaturas.git
-cd flask_temperaturas
+git clone https://github.com/DAW-distancia/Weather-App.git
+cd Weather-App
 python3 -m venv env   # Crea un entorno virtual
 source env/bin/activate  # Activa el entorno virtual
 pip install -r requirements.txt
 
+cp /vagrant/inicio /var/www/Weather-App/weather_app/__init__.py
 # creación wsgi.py
 cat <<EOF > wsgi.py
-from app import app as application
+from weather_app import app as application
 
 EOF
 
 # Configuración de apache default. Añadimos las líneas al default.
 cat <<EOF >> /etc/apache2/sites-available/000-default.conf
-    WSGIDaemonProcess flask_temp python-path=/var/www/flask_temperaturas:/var/www/flask_temperaturas/env/lib/python3.10/site-packages
+    WSGIDaemonProcess flask_temp python-path=/var/www/Weather-App:/var/www/Weather-App/env/lib/python3.10/site-packages
     WSGIProcessGroup flask_temp
-    WSGIScriptAlias / /var/www/flask_temperaturas/wsgi.py process-group=flask_temp
-    <Directory /var/www/flask_temperaturas>
+    WSGIScriptAlias / /var/www/Weather-App/wsgi.py process-group=flask_temp
+    <Directory /var/www/Weather-App>
             Require all granted
     </Directory>
 EOF
 
 # Permisos para apache
-chown -R www-data:www-data /var/www/flask_temperaturas
+
+chown -R www-data:www-data /var/www/Weather-App
+
 
 # Reinicio apache 
 systemctl restart apache2
